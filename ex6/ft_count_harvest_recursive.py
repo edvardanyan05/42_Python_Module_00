a28c78d24e09c25b5f3a5533c5609a2bf9977615
Edmond Vardanyan <edmondvar2005@gmail.com>
2026-05-06 21:30:12 +0400

    ex6

---
+++ ex6/ft_count_harvest_recursive.py
@@ -0,0 +1,9 @@
+def ft_count_harvest_recursive():
+    days = int(input("Days until harvest: "))
+    def helper(current):
+        if days < current:
+            print("Harvest time!")
+            return
+        print(f"Day {current}")
+        helper(current + 1)
+    helper(1)
